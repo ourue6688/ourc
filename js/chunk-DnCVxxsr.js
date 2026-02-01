189d96c2d1ebd81bed803f239ff669f947cc2fe4
LOG1997 <LOG1997@users.noreply.github.com>
2026-02-01 03:08:18 +0000

    deploy: b7181a9f323d81cb75a60033706b334264860833

---
+++ js/chunk-DnCVxxsr.js
@@ -0,0 +1,1 @@
+import{a as r}from"./chunk-Ddo6ZDlq.js";import{o,Q as s,P as t}from"./chunk-Bpe8Il_K.js";const n=r({},[["render",function(r,n){const a=t("router-view");return o(),s(a)}]]);export{n as default};
